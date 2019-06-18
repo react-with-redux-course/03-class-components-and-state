3623d806f2a019042339583401c0de40a47fea34
Grace Lee <igracelee1@gmail.com>
2019-06-18 12:48:08 -0400

    refactor with ternaries and season object

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Brrr it's cold",
+        iconName: 'snowflake'
+    }
+}
+
+// determine what the season is using the following ternaries
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+ }
+
 const SeasonDisplay = (props) => {
-    return <div>Season Display</div>
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName } = seasonConfig[season];
+    // const text = season === 'winter' ? 'Burr, it is chilly' : 'Let\'s hit the beach';
+    // const icon = season === 'winter' ? 'snowflake' : 'sun';
+    return (
+        <div>
+            <i className={`${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`${iconName} icon`} />
+            
+        </div>
+    )
 };
 
 export default SeasonDisplay;
